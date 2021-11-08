ddcf711c0c926b4dbd4d230fe7ee927bab4dd642
bariskayhan93 <bariskayhan53@gmail.com>
2021-11-08 02:31:01 +0100

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about page</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae sapiente,
+        libero inventore quibusdam, voluptates voluptate doloremque dolor
+        dolorem laboriosam, aliquid doloribus beatae a fugit reiciendis sit
+        excepturi similique distinctio ab asperiores voluptatem veniam sunt!
+        Maiores vel possimus in nobis harum eum similique consequuntur! Tempore
+        eius earum dolor eaque qui. Doloremque iure eaque itaque aut cupiditate
+        magnam repudiandae ad consequatur distinctio quos quam expedita
+        reprehenderit est voluptas adipisci aliquam ducimus esse accusantium,
+        ratione deleniti veritatis dolore eligendi facere. Laudantium quam,
+        optio nulla doloribus rem repudiandae deleniti praesentium error ullam
+        fugit eveniet, cupiditate provident magni quisquam esse hic? Iste
+        aliquam deserunt ea.
+      </p>
+    </section>
+  );
+};
+
+export default About;
